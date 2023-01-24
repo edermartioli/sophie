52c009f3770fe0607cf866fe3c40e3b0e3360cf0
Eder <edermartioli@gmail.com>
2023-01-24 17:39:56 -0300

    Add files via upload

--- sophie_timeseries_analysis.py
+++ sophie_timeseries_analysis.py
@@ -28,7 +28,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 from astropy.io import ascii
 import corner
-from balrogo import marginals
+#from balrogo import marginals
+
 parser = OptionParser()
 parser.add_option("-i", "--input", dest="input", help="Input file name",type='string',default="")
 parser.add_option("-p", action="store_true", dest="plot", help="plot", default=False)
@@ -45,8 +46,8 @@ specdata = ascii.read(options.input, data_start=1)
 bjd = np.array(specdata['bjd'])
 ns = len(bjd)
 
-#select_cols = ['snr','berv','airmass','vrad','biss','fwhm','sindex','H-alpha','Na I','CaI']
-select_cols = ['berv','vrad','biss','fwhm','sindex','H-alpha','Na I','CaI']
+#select_cols = ['snr','berv','airmass','vrad','biss','fwhm','sindex','H-alpha','NaI','CaI']
+select_cols = ['berv','vrad','biss','fwhm','sindex','H-alpha','NaI','CaI']
 
 slabels, labels = [], []
 for col in specdata.columns :
@@ -61,9 +62,8 @@ for i in range(ns) :
         samples[i].append(np.array(specdata[slabels[j]])[i])
 samples = np.array(samples, dtype=float)
 
-#fig = marginals.corner(samples,labels=slabels,quantiles=[0.5],show_titles=True)
-fig = marginals.corner(samples,labels=slabels,quantiles=[0.16, 0.5, 0.84])
-#fig = corner.corner(samples, show_titles=True, labels = newlabels, plot_datapoints=True, quantiles=[0.16, 0.5, 0.84], truths=truths, labelsize=12, labelpad=2.0)
+#fig = marginals.corner(samples,labels=slabels,quantiles=[0.16, 0.5, 0.84])
+fig = corner.corner(samples, show_titles=True, labels = newlabels, plot_datapoints=True, quantiles=[0.16, 0.5, 0.84], truths=truths, labelsize=12, labelpad=2.0)
 
 for ax in fig.get_axes():
     plt.setp(ax.get_xticklabels(), ha="left", rotation=45)
